d90772ae36a2fe506cf371212cfc07c608b012b4
Athelbiban <athelbiban@gmail.com>
2022-08-03 20:42:02 +0300

    Книга проработана до 209 страницы.

--- BilLyubanovich_book/p1_c10.py
+++ BilLyubanovich_book/p1_c10.py
@@ -1,20 +1,15 @@
-class Cat:
+class Person:
     def __init__(self, name):
         self.name = name
 
 
-# a_cat = Cat()
-# print(a_cat)
-# another_cat = Cat()
-# print(another_cat)
-# a_cat.age = 3
-# a_cat.name = "Mr. Fuzzybuttons"
-# a_cat.nemesis = another_cat
-# print(a_cat.age)
-# print(a_cat.name)
-# print(a_cat.nemesis)
-# a_cat.nemesis.name = "Mr. Bigglesworth"
-# print(a_cat.nemesis.name)
+class EmailPerson(Person):
+    def __init__(self, name, email):
+        super().__init__(name)
+        self.email = email
 
-furball = Cat('Grumpy')
-print(furball.name)
+
+bob = EmailPerson('Bob Frapples', 'bob@frapples.com')
+
+print(bob.name)
+print(bob.email)
