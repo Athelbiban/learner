ec46a2ee5b72e6b941a0235a5ad5e1a05c1e5b15
Athelbiban <athelbiban@gmail.com>
2022-08-05 10:29:33 +0300

    Добавлено решение №3 задачи at_3.6

--- stepic_at/at_3.6.py
+++ stepic_at/at_3.6.py
@@ -9,12 +9,28 @@
 #             counter += 1
 
 
-# Список представляющий собой заданную последовательность,
-# после чего выводящий прервый n элементов.
-n = int(input())
-lst = []
+# # Список представляющий собой заданную последовательность,
+# # после чего выводящий прервый n элементов.
+# n = int(input())
+# lst = []
+#
+# for k in range(1, n + 1):
+#     for _ in range(k):
+#         lst.append(k)
+# print(lst[:n])
 
-for k in range(1, n + 1):
-    for _ in range(k):
-        lst.append(k)
-print(lst[:n])
+
+# Функция-генератор которая создаёт бесконечную последовательность,
+# и выводит только n членов этой последовательности.
+def infinite_sequence(n):
+    counter = 0
+    for i in range(1, n + 1):
+        k = 0
+        while counter < n and k < i:
+            counter += 1
+            k += 1
+            yield i
+
+
+if __name__ == '__main__':
+    print(*infinite_sequence(10))
