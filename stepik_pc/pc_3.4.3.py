a0c1d5f3817fd2b1045d2cfb5d134cacb64343d1
Athelbiban <athelbiban@gmail.com>
2022-08-13 20:01:20 +0300

    Улучшил решение задачи pc_3.4.3

--- stepik_pc/pc_3.4.3.py
+++ stepik_pc/pc_3.4.3.py
@@ -1,19 +1,7 @@
-with open(r'files\dataset_3363_3.txt') as file:
-    s, d = [], {}
-    for i in file:
-        s += i.strip().lower().split()
+with open(r'files\dataset_3363_3(1).txt') as inf, open(r'files\answer.txt', 'w') as ouf:
+    d = {}
+    s = sorted(inf.read().strip().lower().split())
     for i in s:
         d[i] = d.get(i, 0) + 1
-
     d = sorted(d.items(), key=lambda x: x[1], reverse=True)
-    m = 0
-
-    for i, e in enumerate(d):
-        if e[1] < m:
-            del d[i:]
-        else:
-            m = e[1]
-    d = sorted(d, key=lambda x: x[0])
-
-with open(r'files\answer.txt', 'w') as file:
-    file.write(f'{d[0][0]} {d[0][1]}')
+    ouf.write(f'{d[0][0]} {d[0][1]}')
