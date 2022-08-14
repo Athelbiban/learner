5a9c0571174c14d490391ec41883256b55cd89fd
Athelbiban <athelbiban@gmail.com>
2022-08-14 18:08:13 +0300

    Улучшил решение задачи 3.4.4 из курса Stepik Python

--- stepik_pc/pc_3.4.4.py
+++ stepik_pc/pc_3.4.4.py
@@ -1,11 +1,10 @@
-with open(r'files\dataset_3363_4.txt') as inf, open(r'files\answer.txt', 'w') as ouf:
-    d, s = {}, [0, 0, 0]
+with open(r'files\test_doc.txt') as inf, open(r'files\answer.txt', 'w') as ouf:
+    s, c = [0, 0, 0], 0
     for line in inf:
         line = line.strip().split(';')
         ouf.write(f'{(int(line[1]) + int(line[2]) + int(line[3])) / 3}\n')
-        d[line[0]] = d.get(line[0], [int(i) for i in line[1:]])
-    for i in d.values():
-        s[0] += i[0]
-        s[1] += i[1]
-        s[2] += i[2]
-    ouf.write(f'{s[0] / len(d)} {s[1] / len(d)} {s[2] / len(d)}')
+        s[0] += int(line[1])
+        s[1] += int(line[2])
+        s[2] += int(line[3])
+        c += 1
+    ouf.write(f'{s[0] / c} {s[1] / c} {s[2] / c}')
