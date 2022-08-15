744ae52aa8edb53d62e5d3475088ea153638ca2d
Athelbiban <athelbiban@gmail.com>
2022-08-15 13:04:17 +0300

    Удалил ненужные файлы

--- test_00.py
+++
@@ -1,6 +0,0 @@
-cords = list(map(str,[111,211,311,121,221,321,131,231,331,112,212,312,122,222,322,132,232,332,113,213,313,123,223,323,133,233,333]))
-alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
-dic = {a:c for a,c in zip(alpha, cords)}
-print(cords)
-print(dic)
-print(alpha[25])
