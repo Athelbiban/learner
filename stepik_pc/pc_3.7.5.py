c8b759c5413ffa851bc5a52958cf00118d7c99c4
Athelbiban <athelbiban@gmail.com>
2022-08-17 12:34:05 +0300

    Решена последняя задача 3.7.5 из курса Stepik 'Python курс для начинающих'. Прогресс по курсу: 100\100.

---
+++ stepik_pc/pc_3.7.5.py
@@ -0,0 +1,8 @@
+with open(r'files/dataset_3380_5.txt', encoding='utf-8') as inf, open(r'files/answer.txt', 'w', encoding='utf-8') as ouf:
+    d, s = {str(i): '-' for i in range(1, 12)}, []
+    for i in inf:
+        c, _, h = i.split()
+        h, s = int(h), s + [c]
+        d[c] = h if d[c] == '-' else d[c] + h
+    for k, v in d.items():
+        ouf.write(f'{k} {v / s.count(k) if v != "-" else v}\n')
