79f0d2aec5f9f90eafb252c2801b27cb233dca46
Athelbiban <athelbiban@gmail.com>
2022-08-21 20:51:57 +0300

    Удалил папку /delete и переименовал /euler_task в /projecteuler.net

--- delete/test_10.py
+++
@@ -1,7 +0,0 @@
-import re
-
-#получаем серии (в виде списка кортежей)
-series = re.findall(r'((\w)\2*)', 'aaabccccCCaB')
-print(series)
-#формируем строку, удаляя длины, равные 1
-print(''.join([re.sub(r'\b1\b', '',str(len(crt[0])))+crt[1] for crt in series]))
--- delete/test_4.py
+++
@@ -1,6 +0,0 @@
-from solution import fib
-
-print(fib(3))  # 2
-print(fib(5))  # 5
-print(fib(10))  # 55
-print(fib(38))  # 38
