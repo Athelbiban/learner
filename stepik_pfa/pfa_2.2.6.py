97aafbd83731c0738d287863021d30f66b0a50c8
Athelbiban <athelbiban@gmail.com>
2022-08-23 20:03:53 +0300

    Дорешал задачу pfa_2.2.6

--- stepik_pfa/pfa_2.2.6.py
+++ stepik_pfa/pfa_2.2.6.py
@@ -1,20 +1,23 @@
-n = int(input())
-s = [int(input()) for _ in range(n)]
+s = [int(input()) for _ in range(int(input()))]
 composition = int(input())
 
-# for i, e in enumerate(s):
-#     for k in range(i + 1, len(s)):
-#         if e * s[k] == composition:
-#             print("ДА")
+for i, e in enumerate(s):
+    for k in range(i + 1, len(s)):
+        if e * s[k] == composition:
+            exit(print("ДА"))
+print("НЕТ")
+
+
+# # Вариант без вложенных циклов:
+# n = int(input())
+# s = [int(input()) for _ in range(n)]
+# composition = int(input())
+# counter = 0
+# for i in s:
+#     if i != 0 and int(composition / i) in s:
+#         counter += 1
+#     if counter > 1:
+#         print("ДА")
+#         break
 # else:
 #     print("НЕТ")
-
-counter = 0
-for i in s:
-    if i != 0 and int(composition / i) in s:
-        counter += 1
-    if counter > 1:
-        print("ДА")
-        break
-else:
-    print("НЕТ")
