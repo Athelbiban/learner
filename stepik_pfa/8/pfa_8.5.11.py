4f4eb3e6f6f8b2b6844ffe6adad79c4b8eb1bfbd
Athelbiban <athelbiban@gmail.com>
2022-09-14 16:13:59 +0300

    add pfa_8.4.12.py, pfa_8.4.13.py, pfa_8.4.14.py, pfa_8.4.15.py, pfa_8.4.16.py, pfa_8.5.11.py, pfa_8.5.12.py

---
+++ stepik_pfa/8/pfa_8.5.11.py
@@ -0,0 +1,4 @@
+# for _ in range(int(input())):
+#     print(len(set(input().lower())))
+
+[print(len(set(input().lower()))) for _ in range(int(input()))]
---
+++ stepik_pfa/8/pfa_8.5.12.py
@@ -0,0 +1,5 @@
+n = int(input())
+a = set()
+a = set(a.add(input().lower()) for _ in range(n))
+
+print(a)
