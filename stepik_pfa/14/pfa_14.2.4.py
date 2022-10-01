b465cb5221a61b23663bea88f233efcfa6151812
Athelbiban <athelbiban@gmail.com>
2022-10-01 12:13:08 +0300

    changed pfa_14.2.4.py

--- stepik_pfa/14/pfa_14.2.4.py
+++ stepik_pfa/14/pfa_14.2.4.py
@@ -1,14 +1,26 @@
 import turtle as t
+import math
 
-n = 8
-length = 200
+numbers_web_ray = 8  # Число лучей паутины кратное 360
+web_rings = 5
+length_ray = 200
+ind = length_ray // (web_rings + 1)
+a = 90 / (numbers_web_ray / 4)
+b = (180 - a) / 2
+c = 180 - b
 
-t.dot(15)
-for j in range(0, 360, 360 // n):
+t.dot(10)
+for j in range(0, 360, 360 // numbers_web_ray):
     t.setheading(j)
-    t.fd(length)
+    t.fd(length_ray)
     t.stamp()
     t.lt(180)
-    t.fd(length)
+    t.fd(length_ray)
 
-
+for i in range(1, web_rings + 1):
+    t.setheading(0)
+    t.fd(ind)
+    t.lt(c)
+    for _ in range(numbers_web_ray):
+        t.fd(math.sqrt(2*(ind*i)**2 - 2*(ind*i)**2*math.cos(a/360*math.pi*2)))
+        t.lt(a)
