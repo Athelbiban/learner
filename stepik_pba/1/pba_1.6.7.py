88c10c962084ddc4e1b3822b3dbbed8071250ae6
athelbiban <vostrovso@gmail.com>
2022-11-01 16:08:37 +0300

    change pba_1.6.7.py

--- stepik_pba/1/pba_1.6.7.py
+++ stepik_pba/1/pba_1.6.7.py
@@ -15,18 +15,17 @@ G : F
 """
 
 
-def class_inheritance(ancestor, descendant):
-    global d
-
-    def func(u):
-        if u == ancestor:
-            return True
-        return class_inheritance(ancestor, u)
-
-    if descendant == 'object':
+def class_inheritance(end, start, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not d[start]:
         return False
-    for k in d[descendant]:
-        func(k)
+    for anc in d[start]:
+        if anc not in path:
+            newpath = class_inheritance(end, anc, path)
+            if newpath:
+                return True
     return False
 
 
@@ -38,8 +37,11 @@ for n in range(int(input())):
         del lst[1]
         d[lst[0]] = d.get(lst[0], []) + lst[1:]
     else:
-        d[lst[0]] = d.get(lst[0], ['object'])
+        d[lst[0]] = d.get(lst[0], [])
 
 for q in range(int(input())):
     a, b = input().split()
-    print(('No', 'Yes')[class_inheritance(a, b)])
+    c = True
+    if a != b:
+        c = class_inheritance(a, b)
+    print(('No', 'Yes')[c])
