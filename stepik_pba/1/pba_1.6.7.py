2263363647898cc2e6ece5f0d77b5d5b5480e06d
athelbiban <vostrovso@gmail.com>
2022-11-01 20:07:11 +0300

    change pba_1.6.7.py

--- stepik_pba/1/pba_1.6.7.py
+++ stepik_pba/1/pba_1.6.7.py
@@ -1,47 +1,27 @@
-"""
-12
-A
-X
-B : A
-C : A
-Y : A X
-Z : X
-D : B C
-V : Y Z
-E : D
-F : D
-W : V
-G : F
-"""
-
-
-def class_inheritance(end, start, path=[]):
-    path = path + [start]
-    if start == end:
+def class_inheritance(parent, child, path=[]):
+    path = path + [child]
+    if child == parent:
         return path
-    if not d[start]:
+    if not d[child]:
         return False
-    for anc in d[start]:
-        if anc not in path:
-            newpath = class_inheritance(end, anc, path)
+    for par in d[child]:
+        if par not in path:
+            newpath = class_inheritance(parent, par, path)
             if newpath:
                 return True
     return False
 
 
 d = {}
+for _ in range(int(input())):
+    s = input().split()
+    d[s[0]] = d.get(s[0], []) + s[2:]
 
-for n in range(int(input())):
-    lst = input().split()
-    if len(lst) > 1:
-        del lst[1]
-        d[lst[0]] = d.get(lst[0], []) + lst[1:]
-    else:
-        d[lst[0]] = d.get(lst[0], [])
-
-for q in range(int(input())):
+lst = []
+for _ in range(int(input())):
     a, b = input().split()
-    c = True
+    flag = True
     if a != b:
-        c = class_inheritance(a, b)
-    print(('No', 'Yes')[c])
+        flag = class_inheritance(a, b)
+    lst.append(flag)
+[print(('No', 'Yes')[f]) for f in lst]
