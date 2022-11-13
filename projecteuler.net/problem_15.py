b9de0618ec014b8a2f4a8202aa6abd4708692de5
athelbiban <vostrovso@gmail.com>
2022-11-13 20:09:45 +0300

    feat: problem_15.py

--- projecteuler.net/problem_15.py
+++ projecteuler.net/problem_15.py
@@ -1,49 +1,37 @@
-import random as rnd
-from time import time
-
-start = time()
+import time
 
 
-def way(n):
-    lst = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    lst_inv = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
-    i, j = 0, 0
-    lst[i][j], lst_inv[j][i] = 1, 1
-    while i + j != 2 * n:
-        x, y = rnd.choice(((0, 1), (1, 0)))
-        if i + x <= n and j + y <= n:
-            i, j = i + x, j + y
-            lst[i][j] = 1
-            lst_inv[j][i] = 1
-    return lst, lst_inv
+def ways(graph, goal, start='00'):
+    stack = [(start, [start])]
+    count = 0
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in graph[vertex]:
+            if next == goal:
+                # yield path + [next]
+                count += 1
+            else:
+                stack.append((next, path + [next]))
+    return count
 
 
 def main():
-    n = 3
-    ways = []
-    points_val = 0
-    for _ in range(10_000):
-        w, w_inv = way(n)
-        if w not in ways:
-            ways.append(w)
-            ways.append(w_inv)
-
-    # for v in ways:
-    #     if v[0][1] == 1:
-    #         points_val += 1
-    #     print(points_val)
-
-    for i, k in enumerate(ways, 1):
-        if k[1][2] == 1:
-            points_val += 1
-    print(points_val)
-    #     print(i)
-    #     print(*k, sep='\n')
-    #     print()
-    # print(ways)
-    # print(len(ways))
+    n, m = 9, 9
+    d = {}
+    for i in range(n+1):
+        for j in range(m+1):
+            if i <= n-1 and j <= m-1:
+                d[f'{str(i)}{str(j)}'] = f'{str(i)}{str(j + 1)}', f'{str(i + 1)}{str(j)}'
+            elif i <= n-1 and j > m-1:
+                d[f'{str(i)}{str(j)}'] = f'{str(i + 1)}{str(j)}',
+            elif i > n-1 and j <= m-1:
+                d[f'{str(i)}{str(j)}'] = f'{str(i)}{str(j + 1)}',
+            else:
+                d[f'{str(i)}{str(j)}'] = ()
+    return ways(d, f'{str(n)}{str(m)}')
 
 
 if __name__ == '__main__':
-    main()
-print(time()-start)
+    start_time = time.time()
+    print(main())
+    print(time.time() - start_time)
