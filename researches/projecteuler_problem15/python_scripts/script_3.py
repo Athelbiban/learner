5bc45bf3e5591450ae7355f0d5cf2a37211eea3f
athelbiban <vostrovso@gmail.com>
2022-11-23 16:11:02 +0300

    feat: script_3.py

--- researches/projecteuler_problem15/python_scripts/script_3.py
+++ researches/projecteuler_problem15/python_scripts/script_3.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python3
-
-
+with open('out_1.txt', encoding='utf-8') as inf1,\
+        open('out_3.txt') as inf2,\
+        open('out_4.txt', 'w', encoding='utf-8') as ouf:
+    # for i in inf2:
+    #     i = i.split()
+    #     i1 = []
+    #     for e in i:
+    #         e = e.replace('-', '')
+    #         i1.append(e)
+    #     print(*i1, file=ouf)
+    a = set(inf1.read().split('\n'))
+    b = set(inf2.read().split('\n'))
+    c = b - a
+    for i in sorted(c):
+        print(i, file=ouf)
 
