499a5e1e606318bb6c67326c1d106213f5414f91
athelbiban <vostrovso@gmail.com>
2022-12-20 20:47:50 +0300

    fix: mysql.sql, postgresql.sql

---
+++ SQL/postgresql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE regions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    active BOOLEAN NOT NULL DEFAULT TRUE 
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    regions_id INTEGER NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE, 
+    FOREIGN KEY (regions_id) REFERENCES regions(id)
+);
