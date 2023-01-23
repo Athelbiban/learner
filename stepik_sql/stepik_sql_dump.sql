be19cee6c0bdb8f8aad1b75cb28b6fe1ca425cf6
athelbiban <vostrovso@gmail.com>
2023-01-23 15:46:38 +0300

    fix: stepik_sql_dump.sql
        add table trip

--- stepik_sql/stepik_sql_dump.sql
+++ stepik_sql/stepik_sql_dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: stepik_sql
 -- ------------------------------------------------------
--- Server version	8.0.31-0ubuntu0.22.04.1
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -68,6 +68,34 @@ LOCK TABLES `supply` WRITE;
 INSERT INTO `supply` VALUES (1,'Лирика','Пастернак Б.Л.',518.99,2),(3,'Белая гвардия','Булгаков М.А.',540.50,7);
 /*!40000 ALTER TABLE `supply` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `trip`
+--
+
+DROP TABLE IF EXISTS `trip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trip` (
+  `trip_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `per_diem` decimal(8,2) DEFAULT NULL,
+  `date_first` date DEFAULT NULL,
+  `date_last` date DEFAULT NULL,
+  PRIMARY KEY (`trip_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trip`
+--
+
+LOCK TABLES `trip` WRITE;
+/*!40000 ALTER TABLE `trip` DISABLE KEYS */;
+INSERT INTO `trip` VALUES (1,'Баранов П.Е.','Москва',700.00,'2020-01-12','2020-01-17'),(2,'Абрамова К.А.','Владивосток',450.00,'2020-01-14','2020-01-27'),(3,'Семенов И.В.','Москва',700.00,'2020-01-23','2020-01-31'),(4,'Ильиных Г.Р.','Владивосток',450.00,'2020-01-12','2020-02-02'),(5,'Колесов С.П.','Москва',700.00,'2020-02-01','2020-02-06'),(6,'Баранов П.Е.','Москва',700.00,'2020-02-14','2020-02-22'),(7,'Абрамова К.А.','Москва',700.00,'2020-02-23','2020-03-01'),(8,'Лебедев Т.К.','Москва',700.00,'2020-03-03','2020-03-06'),(9,'Колесов С.П.','Новосибирск',450.00,'2020-02-27','2020-03-12'),(10,'Семенов И.В.','Санкт-Петербург',700.00,'2020-03-29','2020-04-05'),(11,'Абрамова К.А.','Москва',700.00,'2020-04-06','2020-04-14'),(12,'Баранов П.Е.','Новосибирск',450.00,'2020-04-18','2020-05-04'),(13,'Лебедев Т.К.','Томск',450.00,'2020-05-20','2020-05-31'),(14,'Семенов И.В.','Санкт-Петербург',700.00,'2020-06-01','2020-06-03'),(15,'Абрамова К.А.','Санкт-Петербург',700.00,'2020-05-28','2020-06-04'),(16,'Федорова А.Ю.','Новосибирск',450.00,'2020-05-25','2020-06-04'),(17,'Колесов С.П.','Новосибирск',450.00,'2020-06-03','2020-06-12'),(18,'Федорова А.Ю.','Томск',450.00,'2020-06-20','2020-06-26'),(19,'Абрамова К.А.','Владивосток',450.00,'2020-07-02','2020-07-13'),(20,'Баранов П.Е.','Воронеж',450.00,'2020-07-19','2020-07-25');
+/*!40000 ALTER TABLE `trip` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -78,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-22 19:29:56
+-- Dump completed on 2023-01-23 15:45:22
