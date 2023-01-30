28a8bdfd95b10370bb31c7a24da387e3d53b7be4
athelbiban <vostrovso@gmail.com>
2023-01-30 10:01:14 +0300

    feat: md5.py, test.csv

--- researches/md5/md5.py
+++ researches/md5/md5.py
@@ -1,17 +1,26 @@
 import csv
 
 
-with open(r'file.txt', encoding='utf-8') as inf, open(r'test.csv', 'w', encoding='utf-8') as ouf:
+code = {
+    ' СБ': 'Сборочный чертёж',
+    ' Е1': 'Схема деления структурная',
+    ' Э3': 'Схема электрическая принципиальная',
+    'ПЭ3': 'Перечень элементов',
+    ' ТБ': 'Таблица соединений',
+    ' ВП': 'Ведомость покупных изделий',
+    ' МЧ': 'Монтажный чертёж'
+}
+
+with open(r'1ВЛ.md5', encoding='utf-8') as inf, open(r'test.csv', 'w', encoding='utf-8') as ouf:
     for row in inf:
         row1 = row.split(maxsplit=1)
-        row2, row3 = row1[1].split('_')
-        decimal, name = row2[1:-1], row3[1:-5]
+        row2, row3 = row1[1].split(' _ ')
+        decimal, name = row2[1:], row3[:-5]
         md5 = row1[0]
-        if not decimal[-3:].isdigit():
-            if decimal[-3:] == ' СБ':
-                name = f'{name} Сборочный чертёж'
-            elif decimal[-3:] == 'ПЭ3':
-                name = f'{name} Перечень элементов'
+        decimal_code = decimal[-3:]
+        if not decimal_code.isdigit():
+            if decimal_code in code:
+                name = f'{name} {code[decimal_code]}'
         lst = [md5, decimal, name]
-        wr = csv.writer(ouf, delimiter=',')
+        wr = csv.writer(ouf, delimiter='\t', lineterminator='\r')
         wr.writerow(lst)
