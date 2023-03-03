cadbc258539f815b62a7159a3c22329542694f88
athelbiban <vostrovso@gmail.com>
2023-03-03 21:38:52 +0300

    end: sql_3.5.11.sql (finish)

--- stepik_sql/3/sql_3.5.11.sql
+++ stepik_sql/3/sql_3.5.11.sql
@@ -1,22 +1,24 @@
 WITH cte1 (st_name, lesson, sub_time)
     AS (SELECT student_name,
                CONCAT(module_id, '.', lesson_position),
-               FROM_UNIXTIME(submission_time)
-          FROM student s
-                   JOIN step_student ss ON s.student_id = ss.student_id
-                   AND result = 'correct'
-                   JOIN step st USING (step_id)
-                   JOIN lesson l USING (lesson_id)
-         GROUP BY student_name, module_id, lesson_position
-         ORDER BY student_name),
+               MAX(submission_time)
+        FROM student s
+             JOIN step_student ss ON s.student_id = ss.student_id
+             AND result = 'correct'
+             JOIN step st USING (step_id)
+             JOIN lesson l USING (lesson_id)
+        GROUP BY student_name, module_id, lesson_position
+        ORDER BY student_name),
 
-    cte2 (student)
-        AS (SELECT st_name
-            FROM (SELECT st_name, COUNT(lesson) AS count_lesson
-                  FROM cte1
-                  GROUP BY st_name) AS q
-            WHERE count_lesson = 3)
+cte2 AS (SELECT st_name
+         FROM cte1
+         GROUP BY st_name
+         HAVING COUNT(lesson) = 3)
 
-SELECT *
-FROM cte1
-WHERE st_name IN (SELECT * FROM cte2)
+SELECT c1.st_name AS Студент,
+       lesson AS Урок,
+       FROM_UNIXTIME(sub_time) AS Макс_время_отправки,
+       IFNULL(CEIL(
+           (sub_time - LAG(sub_time) OVER(PARTITION BY c1.st_name ORDER BY sub_time))
+               / 86400), '-') AS Интервал
+FROM cte1 c1 JOIN cte2 c2 USING(st_name)
