4bd3602285d12c7e958cbbfe91824c5dcaf38e30
athelbiban <vostrovso@gmail.com>
2023-03-19 21:33:33 +0300

    feat: stepik_bookstore_4_1_dump.sql

--- stepik_sql/4/stepik_bookstore_4_1_dump.sql
+++ stepik_sql/4/stepik_bookstore_4_1_dump.sql
@@ -56,7 +56,7 @@ CREATE TABLE `supply` (
   `price` decimal(8,2) DEFAULT NULL,
   `amount` int DEFAULT NULL,
   PRIMARY KEY (`supply_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
