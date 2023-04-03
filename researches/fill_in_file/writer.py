7d9ba33ae939ff42429274c8504a062e88477938
athelbiban <vostrovso@gmail.com>
2023-04-03 18:49:50 +0300

    ref: /researches (add, feat, del)
    	новый файл:    researches/fill_in_file/new_file.txt
    	изменено:      researches/fill_in_file/write_here.txt
    	изменено:      researches/fill_in_file/writer.py
    	удалено:       researches/map/cycle.py
    	удалено:       researches/map/map().py
    	удалено:       researches/standart_deviation/standart_deviation.py

--- researches/fill_in_file/writer.py
+++ researches/fill_in_file/writer.py
@@ -1,7 +1,3 @@
-with open(r'write_here.txt', 'w', encoding='utf-8') as ouf:
-    n = 0
-    for i in range(255):
-        ouf.write('Stas ')
-        n += 1
-        if not n % 15:
-            ouf.write('\n')
+with open('../../stepik_pba/2/dataset_24465_4.txt', encoding='utf-8') as inf,\
+        open('new_file.txt', 'w', encoding='utf-8') as ouf:
+    ouf.write('\n'.join(inf.read().split()[::-1]))
