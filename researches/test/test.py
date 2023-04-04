0e53cfa8c270cf0f285598a96d55807add70a05c
athelbiban <vostrovso@gmail.com>
2023-04-04 13:34:39 +0300

    mod: researches/test/test.py

--- researches/test/test.py
+++ researches/test/test.py
@@ -1,24 +1,48 @@
 import timeit
+import os.path
+import re
+
+
+os.chdir(r'c:\Users\VostrovSO\Downloads')
+directory = os.walk('main')
+reg = re.compile(r'\.py$')
 
 
 def f1():
-    lst = list(range(1000))
-    # lst = []
-    # for i in range(1000):
-    #     lst.append(i)
-    # return lst
+    for path, dirs, files in directory:
+        for file in files:
+            if reg.search(file):
+                break
 
 
 def f2():
-    lst = [i for i in range(1000)]
-    return lst
+    for path, dirs, files in directory:
+        for file in files:
+            if re.search(r'\.py$', file):
+                break
+
+
+def f3():
+    for path, dirs, files in directory:
+        for file in files:
+            if file.endswith('.py'):
+                break
+
+
+def f4():
+    for path, dirs, files in directory:
+        for file in files:
+            if '.py' in file[-3:]:
+                break
 
 
 if __name__ == '__main__':
-    x = timeit.repeat(f1, repeat=3, number=100)
-    y = timeit.repeat(f2, repeat=3, number=100)
+    f1 = timeit.repeat(f1)
+    f2 = timeit.repeat(f2)
+    f3 = timeit.repeat(f3)
+    f4 = timeit.repeat(f4)
 
-    print(x)
-    print(y)
-    print(f'Минимальное значение для f1: {min(x)}')
-    print(f'Минимальное значение для f2: {min(y)}')
+    print(f'Min time re.compile f1: {min(f1)}')
+    print(f'Min time re f2:         {min(f2)}')
+    print(f'Min time endswith f3:   {min(f3)}')
+    print(f'Min time slice f4:      {min(f4)}')
