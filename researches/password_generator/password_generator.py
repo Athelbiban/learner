a5a7c915c4f571e25f396ad50830564e844dfb63
athelbiban <vostrovso@gmail.com>
2023-06-13 17:50:01 +0300

    ref: password_generator.py

--- researches/password_generator/password_generator.py
+++ researches/password_generator/password_generator.py
@@ -2,25 +2,30 @@ import random
 import string
 
 
-def generate_password(length):
+def generate_password(length, char):
     password = []
     # password.extend(random.choice(char) for char in (low, up, dig))
-    password.extend(random.choice(all_char) for _ in range(length))
+    password.extend(random.choice(char) for _ in range(length))
     random.shuffle(password)
     return ''.join(password)
 
 
-def generate_passwords(count, length):
-    return [generate_password(length) for _ in range(count)]
+def generate_passwords(count, length, char):
+    return [generate_password(length, char) for _ in range(count)]
 
 
-if __name__ == '__main__':
+def main():
     low = tuple(set(string.ascii_lowercase) - set('lo'))
     up = tuple(set(string.ascii_uppercase) - set('IO'))
     dig = tuple(string.digits)
     # signs_full = tuple("!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"")
     signs_cut = tuple("!,-.:;?_")
-    low_up_dig = low + up + dig
+    # low_up_dig = low + up + dig
     all_char = low + up + dig + signs_cut
     print(*generate_passwords(int(input('Сколько паролей генерировать: ')),
-                              int(input('Количество символов в пароле: '))), sep='\n')
+                              int(input('Количество символов в пароле: ')),
+                              all_char), sep='\n')
+
+
+if __name__ == '__main__':
+    main()
