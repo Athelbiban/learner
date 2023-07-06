6c521350b2e7ff3fba3ae3dc31f0de0eb9a57779
athelbiban <vostrovso@gmail.com>
2023-07-06 16:04:12 +0300

    ref: get_mail_mailru.py

--- heap/get_mail/get_mail_mailru.py
+++ heap/get_mail/get_mail_mailru.py
@@ -1,24 +1,33 @@
 import imaplib
 import email
 import base64
+import re
 from passwd.mailru import MAIL_PASS, USERNAME
 
 
-mail_pass = MAIL_PASS
-username = USERNAME
-imap_server = 'imap.mail.ru'
-imap = imaplib.IMAP4_SSL(imap_server)
-imap.login(username, mail_pass)
-imap.select('SberBroker')
+def loader_mail_attachments(imap, directory, files_extension='.html'):
+    id_list = imap.search(None, 'ALL')[1][0].split()
+    for next_mail_id in id_list:
+        res, data = imap.fetch(next_mail_id, '(RFC822)')
+        msg = email.message_from_bytes(data[0][1])
+        for part in msg.walk():
+            if part.get_content_disposition() == 'attachment'\
+                    and part.get_filename()[:7] == 'S03DNRY'\
+                    and re.search(r'\.\w+$', part.get_filename()).group() == files_extension:
+                with open(f'{directory}{part.get_filename()}', 'w') as ouf:
+                    ouf.write(base64.b64decode(part.get_payload()).decode())
 
-id_list = imap.search(None, 'ALL')[1][0].split()
-for next_mail_id in id_list:
-    res, data = imap.fetch(next_mail_id, '(RFC822)')
-    msg = email.message_from_bytes(data[0][1])
-    payload = msg.get_payload()
-    for part in msg.walk():
-        if part.get_content_disposition() == 'attachment'\
-                and part.get_filename()[-4:] == 'html'\
-                and part.get_filename()[:7] == 'S03DNRY':
-            with open(f'/home/stas/Загрузки/broker_report/{part.get_filename()}', 'w') as ouf:
-                ouf.write(base64.b64decode(part.get_payload()).decode())
+
+def main():
+    mail_pass = MAIL_PASS
+    username = USERNAME
+    directory = '/home/stas/Загрузки/broker_report/'
+    imap_server = 'imap.mail.ru'
+    imap = imaplib.IMAP4_SSL(imap_server)
+    imap.login(username, mail_pass)
+    imap.select('SberBroker')
+    loader_mail_attachments(imap, directory)
+
+
+if __name__ == '__main__':
+    main()
