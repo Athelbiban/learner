bdc85e61c2041e401f3e80ccb385352035493a60
athelbiban <vostrovso@gmail.com>
2023-07-06 16:04:38 +0300

    feat: parser_broker_report.py

--- heap/broker_report/parser_broker_report.py
+++ heap/broker_report/parser_broker_report.py
@@ -3,11 +3,13 @@ import csv
 import os
 
 
-def broker_report_html_to_csv(input_files, output_file, date=None):
-    header = ['Наименование', 'ISIN', 'Валюта', 'Количество_нп', 'Номинал_нп',
-              'Цена_нп', 'Стоимость_нп', 'НКД_нп', 'Количество_кп',
-              'Номинал_кп', 'Цена_кп', 'Стоимость_кп', 'НКД_кп', 'Количество_изп',
-              'Стоимость_изп', 'Зачисления', 'Списания', 'Остаток', 'Дата']
+def get_portfolio(input_files, output_file, date=None):
+    header = [
+        'Наименование', 'ISIN', 'Валюта', 'Количество_нп', 'Номинал_нп',
+        'Цена_нп', 'Стоимость_нп', 'НКД_нп', 'Количество_кп',
+        'Номинал_кп', 'Цена_кп', 'Стоимость_кп', 'НКД_кп', 'Количество_изп',
+        'Стоимость_изп', 'Зачисления', 'Списания', 'Остаток', 'Дата'
+    ]
     flag1 = True
     for file, date in zip(input_files, date):
         with open(file) as inf, open(output_file, 'a', encoding='utf-8') as ouf:
@@ -28,16 +30,44 @@ def broker_report_html_to_csv(input_files, output_file, date=None):
                         flag1 = False
 
 
+def get_transactions(input_files, output_file):
+    header = [
+        'Дата заключения', 'Дата расчетов', 'Время', 'Наименование', 'Код',
+        'Валюта', 'Вид', 'Количество', 'Цена', 'Сумма', 'НКД', 'Комиссия Брокера',
+        'Комиссия Биржи', 'Номер сделки', 'Комментарий', 'Статус'
+    ]
+    for file in input_files:
+        flag1 = True
+        n = 0
+        with open(file, encoding='utf-8') as inf, open(output_file, 'w', encoding='utf-8') as ouf:
+            writer = csv.writer(ouf)
+            soup = BeautifulSoup(inf.read(), 'html.parser').select('tr')
+            flag2 = False
+            for string in soup:
+                string = string.find_all('td')
+                if string[0].text == 'Итого, RUB':
+                    n += 1
+                    if n >= 2:
+                        break
+                if string[0].text == 'Дата заключения':
+                    flag2 = True
+                if flag2:
+                    if string[0].text not in ['Площадка: Фондовый рынок']:
+                        writer.writerow([elem.text.replace(' ', '') for elem in string])
+
+
 def parse_directory(directory):
     return [f'{directory}{node}' for node in sorted(os.listdir(directory))]
 
 
 def main():
     directory = '/home/stas/Загрузки/broker_report/'
-    paths = parse_directory(directory)
-    out_file = 'parser_broker_report.csv'
+    directory_1 = 'c:\\Users\\VostrovSO\\Downloads\\broker_report\\'
+    paths = parse_directory(directory_1)
+    out_file_1, out_file_2 = ['portfolio.csv', 'transactions.csv']
     date = [f'20{i[-2:]}-{i[2:4]}-{i[:2]}' for i in [path.split('_')[-2] for path in paths]]
-    broker_report_html_to_csv(paths, out_file, date)
+    # get_portfolio(paths, out_file_1, date)
+    get_transactions(paths, out_file_2)
 
 
 if __name__ == '__main__':
